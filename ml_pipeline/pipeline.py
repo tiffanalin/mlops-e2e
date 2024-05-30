af566d245224acf1f7bda59b5570f60709e630fc
[tiffany <tiffany2626lin@gmail.com>
2024-05-30 18:11:35 +0200

    modified TrainingInstanceType

--- ml_pipeline/pipeline.py
+++ ml_pipeline/pipeline.py
@@ -114,7 +114,7 @@ def get_pipeline(
         name="ProcessingInstanceType", default_value="ml.m5.xlarge"
     )
     training_instance_type = ParameterString(
-        name="TrainingInstanceType", default_value="ml.m5.xlarge"
+        name="TrainingInstanceType", default_value="ml.m5.large"
     )
     model_approval_status = ParameterString(
         name="ModelApprovalStatus", default_value="Approved"
